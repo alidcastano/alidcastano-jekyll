142f4931d7d9a6082a80438b16e7120ce9551bbd
Alid Castano <alidcastano@gmail.com>
2016-06-02 17:22:08 -0500

    updated header and footer fade in js feature

--- js/posts.js
+++ js/posts.js
@@ -8,33 +8,6 @@ $(window).bind('scroll', function () {
 });
 
 
-// so that when you reach footer appears and header/nav dissapears 
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    $(".fade-in").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if ((objectBottom - 25) < windowBottom) { //object comes into view (scrolling down)
-
-        if ($(this).css("opacity")==0) {
-          $(this).fadeTo(500,1);
-          $('.site-header').hide();
-        }
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {
-          $(this).fadeTo(500,0);
-          $('.site-header').show();
-        }
-      }
-    });
-  }); $(window).scroll(); //invoke scroll-handler on page-load
-});
-
-
-
 // make all markdown links are external 
 $(document).ready(function() {
    $('a').each(function() {
@@ -45,6 +18,72 @@ $(document).ready(function() {
    });
 });
 
+// 
+//element appears on scroll
+//
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    $(".fade-in").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      
+      /* if the element is completely within bounds of the window, fade it in */
+      if (objectBottom < (windowBottom + 25))  { // object comes into view (scrolling down)
+
+        if ($(this).css("opacity")==0) {
+          $(this).fadeTo(500,1);
+        }
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {
+          $(this).fadeTo(500,0);
+        }
+      }
+    });
+  }); $(window).scroll(); // invoke scroll-handler on page-load
+});
+
+
+// 
+// hide header on on scroll down
+//
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > (navbarHeight + 60)){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
 /*
 
